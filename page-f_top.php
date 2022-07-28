78a6f2cb96a89f594e6fe30dc27300f46646ed1a
fujichi70 <nyq_o0@yahoo.co.jp>
2022-07-28 15:52:34 +0900

    ポートフォリオの詳細ページの加工

--- page-f_top.php
+++ page-f_top.php
@@ -12,10 +12,10 @@ Template Name: F-Top
         <div class="productspage-main--box">
             <div class="productspage-main--item">
                 <div class="productspage-title--box">
-                    <h1 class="productspage-main--title">F Free Board</h1>
-                    <h2 class="productspage-sub--title">日々の悩みを相談する掲示板サイト</h2>
+                    <h1 class="productspage-main--title">Portfolio</h1>
+                    <h2 class="productspage-sub--title">これまで・ここからをつづるサイト</h2>
                 </div>
-                <p class="productspage-sub--text">複雑化したこのご時世で女性もより悩みがちに。<br>そんな日々の生活でのふとした悩みを気軽に投稿できるサイトを作りました。</p>
+                <p class="productspage-sub--text">どんな人物かすぐわかるようなサイトをつくりました。</p>
                 <div class="productspage-btn--box">
                     <a href="#" class="productspage-btn">Web Site</a>
                 </div>
@@ -24,11 +24,11 @@ Template Name: F-Top
             <table class="productspage-table">
                 <tr>
                     <th>使用ツール</th>
-                    <td>Adobe XD / PHP / CSS</td>
+                    <td>Adobe XD / Wordpress / JavaScript / PHP / CSS</td>
                 </tr>
                 <tr>
                     <th>制作期間</th>
-                    <td>25日程度</td>
+                    <td>15日程度</td>
                 </tr>
                 <tr>
                     <th>制作人数</th>
@@ -51,41 +51,24 @@ Template Name: F-Top
                 <i class="fa-solid fa-pencil"></i>
             </div>
             <div class="productspage-point--parts flex">
-                <li class="productspage-point--list"><img class="img" src="<?php echo get_template_directory_uri(); ?>/img/products/top/f-top_category.png" alt="サイトカテゴリー紹介画像"></li>
+                <li class="productspage-point--list"><img class="img" src="<?php echo get_template_directory_uri(); ?>/img/products/top/f-top.png" alt="サイト紹介画像"></li>
                 <div class="productspage-point--texts">
-                    <h4 class="productspage-point--texttitle">カテゴリー</h4>
-                    <p class="productspage-point--text">女性が悩みそうな項目で分けてみました。ログインしなくても閲覧は可能。</p>
+                    <h4 class="productspage-point--texttitle">サイト</h4>
+                    <p class="productspage-point--text">わかりやすくシンプルに一目でわかるように</p>
                 </div>
             </div>
             <div class="productspage-point--parts flex">
-                <li class="productspage-point--list"><img class="img" src="<?php echo get_template_directory_uri(); ?>/img/products/top/f-top_registration.png" alt="会員登録紹介画像"></li>
+                <li class="productspage-point--list"><img class="img" src="<?php echo get_template_directory_uri(); ?>/img/products/top/f-top_skill.png" alt="スキルページ紹介画像"></li>
                 <div class="productspage-point--texts">
-                    <h4 class="productspage-point--texttitle">会員登録画面</h4>
-                    <p class="productspage-point--text">会員登録すると投稿が可能になります。情報を入力するとメールが届き、メールに記載されているURLにアクセスすると登録完了です。</p>
+                    <h4 class="productspage-point--texttitle">スキル部分</h4>
+                    <p class="productspage-point--text">どんなことができるのかをすぐ見てわかるようにつくりました。</p>
                 </div>
             </div>
             <div class="productspage-point--parts flex">
-                <li class="productspage-point--list videos">
-                    <video class="videos" poster="" loop autoplay muted playsinline>
-                        <source src="<?php echo get_template_directory_uri(); ?>/img/products/top/f-top_loginout.mp4" type="video/mp4">
-                        <p>申し訳ございません。動画が再生できませんでした。</p>
-                    </video>
-                </li>
+                <li class="productspage-point--list"><img class="img" src="<?php echo get_template_directory_uri(); ?>/img/products/top/f-top_contact.png" alt="お問い合わせ部分紹介画像"></li>
                 <div class="productspage-point--texts">
-                    <h4 class="productspage-point--texttitle">ログイン・ログアウト</h4>
-                    <p class="productspage-point--text">ログインとログアウト機能の実装です。プロフィール画面も作成しました。</p>
-                </div>
-            </div>
-            <div class="productspage-point--parts flex">
-                <li class="productspage-point--list videos">
-                    <video class="videos" poster="" loop autoplay muted playsinline>
-                        <source src="<?php echo get_template_directory_uri(); ?>/img/products/top/f-top_post.mp4" type="video/mp4">
-                        <p>申し訳ございません。動画が再生できませんでした。</p>
-                    </video>
-                </li>
-                <div class="productspage-point--texts">
-                    <h4 class="productspage-point--texttitle">投稿</h4>
-                    <p class="productspage-point--text">ログインすると投稿できるようになります。投稿されたかわかりやすいように投稿完了の画像も実装しました。</p>
+                    <h4 class="productspage-point--texttitle">お問い合わせ</h4>
+                    <p class="productspage-point--text">どんなことでも簡単にすぐお問い合わせができます。</p>
                 </div>
             </div>
         </div>
@@ -97,8 +80,8 @@ Template Name: F-Top
                 <i class="fa-solid fa-pencil"></i>
             </div>
             <ul>
-                <li>会員登録の際、プロフィール画像をアップロードして表示するのに上手く動作せず時間がかかってしまいました。</li>
-                <li>プロフィールをもっと充実させたり、ユーザーさんがカテゴリーを任意で増やすことができればもっと自由度が上がると思いました。</li>
+                <li>わかりやすくかつデザイン性がなくならないようにバランスを考えるのが難しかったです。</li>
+                <li>これからもいろんなことを学んでサイトを製作し続けていきたいです。</li>
             </ul>
         </div>
     </div>
